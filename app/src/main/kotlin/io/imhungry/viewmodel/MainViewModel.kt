c3078961d545c627458fdcc4a879ca5867a888a1
TripleFrequency <triplefrequency@triplefrequency.com>
2019-02-07 23:20:53 -0500

    Add Injection annotation to MainViewModel

--- app/src/main/kotlin/io/imhungry/viewmodel/MainViewModel.kt
+++ app/src/main/kotlin/io/imhungry/viewmodel/MainViewModel.kt
@@ -1,5 +1,6 @@
 package io.imhungry.viewmodel
 
 import androidx.lifecycle.ViewModel
+import javax.inject.Inject
 
-class MainViewModel: ViewModel()
+class MainViewModel @Inject constructor(): ViewModel()
