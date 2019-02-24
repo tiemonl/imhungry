740622d18fcaba92a1c7c852490395e8c69fe9bf
holden <htwight@gmail.com>
2019-02-24 13:49:20 -0500

    Add basic database
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/kotlin/io/imhungry/db/ImhungryDao.kt
    #	app/src/main/kotlin/io/imhungry/db/ImhungryRepository.kt
    #	app/src/main/kotlin/io/imhungry/model/RestaurantEntity.kt
    #	app/src/main/kotlin/io/imhungry/model/RestaurantTagEntity.kt

--- app/src/main/kotlin/io/imhungry/db/ImhungryDao.kt
+++ app/src/main/kotlin/io/imhungry/db/ImhungryDao.kt
@@ -1,6 +1,36 @@
 package io.imhungry.db
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import dagger.Component
+import io.imhungry.di.RoomModule
+import io.imhungry.model.RestaurantEntity
+import io.imhungry.model.RestaurantTagEntity
+import io.imhungry.model.TagEntity
 
 @Dao
-interface ImhungryDao
+interface ImhungryDao {
+
+    @Insert
+    fun insertTag(tag: TagEntity)
+
+    @get:Query("SELECT * FROM tag")
+    val allTags: LiveData<List<TagEntity>>
+
+    @Insert
+    fun insertRestaurant(restaurant: RestaurantEntity)
+
+    @get:Query("SELECT * FROM restaurant")
+    val allRestaurants: LiveData<List<RestaurantEntity>>
+
+    @Insert
+    fun insertRestaurantTag(restaurantTag: RestaurantTagEntity)
+
+    @get:Query("SELECT * FROM restaurantTag")
+    val allRestaurantTags: LiveData<List<RestaurantTagEntity>>
+
+    @Query("SELECT tag FROM restaurantTag WHERE restaurantId = :restaurantId")
+    fun getTagsForRestaurant(restaurantId: Int): LiveData<List<String>>
+}
