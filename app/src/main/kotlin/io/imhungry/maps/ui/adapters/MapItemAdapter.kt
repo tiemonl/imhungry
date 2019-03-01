ff2e82cda043770edee3a6f8db3c510bc024d0e5
TripleFrequency <triplefrequency@triplefrequency.com>
2019-03-01 00:02:42 -0500

    Animate to clicked restaurant

--- app/src/main/kotlin/io/imhungry/maps/ui/adapters/MapItemAdapter.kt
+++ app/src/main/kotlin/io/imhungry/maps/ui/adapters/MapItemAdapter.kt
@@ -8,14 +8,17 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
 import com.google.maps.model.PlacesSearchResult
 import io.imhungry.R
 
 class MapItemAdapter(
     context: Context,
-    private val items: LiveData<MutableList<PlacesSearchResult>>
-) :
-    RecyclerView.Adapter<MapItemAdapter.RestaurantViewHolder>() {
+    private val items: LiveData<MutableList<PlacesSearchResult>>,
+    private val map: GoogleMap
+) : RecyclerView.Adapter<MapItemAdapter.RestaurantViewHolder>() {
 
     private val inflater = LayoutInflater.from(context)
 
@@ -29,6 +32,7 @@ class MapItemAdapter(
     override fun onBindViewHolder(holder: RestaurantViewHolder, position: Int) {
         val item = items.value?.get(position)
         holder.itemView.setOnClickListener {
+            map.animateCamera(CameraUpdateFactory.newLatLng(item?.geometry?.location?.let { loc -> LatLng(loc.lat, loc.lng) }))
             Toast.makeText(
                 it.context,
                 "Restaurant Name: ${item?.name}",
