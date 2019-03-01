778d0f2b05e7a77707b1446c7eff9b6418112bea
Lucas Combs <l-combs@hotmail.com>
2019-02-28 21:52:16 -0500

    PR suggestion: Moved priority enum to file

---
+++ app/src/main/kotlin/io/imhungry/notifications/NotificationPriority.kt
@@ -0,0 +1,11 @@
+package io.imhungry.notifications
+
+import androidx.core.app.NotificationCompat
+
+enum class NotificationPriority (internal val value: Int) {
+    MIN(NotificationCompat.PRIORITY_MIN),
+    LOW(NotificationCompat.PRIORITY_LOW),
+    DEFAULT(NotificationCompat.PRIORITY_DEFAULT),
+    HIGH(NotificationCompat.PRIORITY_HIGH),
+    MAX(NotificationCompat.PRIORITY_MAX)
+}
