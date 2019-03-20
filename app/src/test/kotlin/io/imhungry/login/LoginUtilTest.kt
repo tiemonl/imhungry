ccffc8699cb6da6d9e3d85f9953a139c2a394089
tiemonl <tiemon.liam@gmail.com>
2019-03-19 21:49:41 -0400

    NOGH: added auth failback back to the project and unit test for failed login

--- app/src/test/kotlin/io/imhungry/login/LoginUtilTest.kt
+++ app/src/test/kotlin/io/imhungry/login/LoginUtilTest.kt
@@ -13,6 +13,9 @@ internal class LoginUtilTest {
     @RelaxedMockK
     private lateinit var mockActivity: AppCompatActivity
 
+    @RelaxedMockK
+    private lateinit var mockFailureCallback: AuthFailureCallback
+
     @BeforeEach
     fun init() {
         MockKAnnotations.init(this)
@@ -22,5 +25,15 @@ internal class LoginUtilTest {
     fun `GIVEN login activity WHEN handle login result THEN verify home launched`() {
         mockActivity.handleLoginActivityResult(LoginConstants.RC_SIGN_IN, AppCompatActivity.RESULT_OK, null)
         verify { mockActivity.startActivity(any()) }
+    }
+
+    @Test
+    fun `GIVEN login activity WHEN handle login failed result THEN verify home launched fail`() {
+        mockActivity.handleLoginActivityResult(
+            LoginConstants.RC_SIGN_IN,
+            AppCompatActivity.RESULT_CANCELED,
+            mockFailureCallback
+        )
+        verify { mockFailureCallback.invoke() }
     }
 }
