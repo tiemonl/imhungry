4f984c05986e7f0682ca6a8be8bf3a90d16821b8
tiemonl <tiemon.liam@gmail.com>
2019-03-25 22:05:55 -0400

    GH-78: added calendar activity to bottom nav and routing

---
+++ app/src/main/java/io/imhungry/calendar/ui/CalendarActivity.kt
@@ -0,0 +1,13 @@
+package io.imhungry.calendar.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.imhungry.R
+
+class CalendarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+    }
+}
