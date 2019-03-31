d9b5d98ce9016e8a7d7cfd719d5c42c37dae20a2
TripleFrequency <triplefrequency@triplefrequency.com>
2019-03-31 00:08:23 -0400

    GH-83: Continue work on firebase repositories

--- app/src/main/kotlin/io/imhungry/firebase/repository/FirebaseReviewRepository.kt
+++ app/src/main/kotlin/io/imhungry/firebase/repository/FirebaseReviewRepository.kt
@@ -2,27 +2,26 @@ package io.imhungry.firebase.repository
 
 import com.google.firebase.firestore.FirebaseFirestore
 import io.imhungry.firebase.model.places.FirebaseReview
+import io.imhungry.firebase.model.user.FirebaseUser
 import javax.inject.Inject
 
 class FirebaseReviewRepository @Inject constructor(
     db: FirebaseFirestore
 ) : FirebaseBaseCollectionRepository(db.collection(FirebaseReview.FIREBASE_COLLECTION_NAME_REVIEWS)) {
 
-    suspend fun getPlaceReviews(placesId: String, limit: Long = FIREBASE_REVIEW_DEFAULT_LIMIT) =
+    private suspend fun queryReviews(field: String, value: String, limit: Long, startAt: Long) =
         performListQuery<FirebaseReview>(
             collection.whereEqualTo(
-                FirebaseReview::placesId.name,
-                placesId
-            ).limit(limit).get()
+                field,
+                value
+            ).limit(limit).startAt(startAt).get()
         )
 
-    suspend fun getUserReviews(userId: String, limit: Long = FIREBASE_REVIEW_DEFAULT_LIMIT) =
-        performListQuery<FirebaseReview>(
-            collection.whereEqualTo(
-                FirebaseReview::author.name,
-                userId
-            ).limit(limit).get()
-        )
+    suspend fun getPlaceReviews(placesId: String, limit: Long = FIREBASE_REVIEW_DEFAULT_LIMIT, startAt: Long = 0) =
+        queryReviews(FirebaseReview::placesId.name, placesId, limit, startAt)
+
+    suspend fun getUserReviews(user: FirebaseUser, limit: Long = FIREBASE_REVIEW_DEFAULT_LIMIT, startAt: Long = 0) =
+        queryReviews(FirebaseReview::author.name, user.userId, limit, startAt)
 
     companion object {
         const val FIREBASE_REVIEW_DEFAULT_LIMIT = 5L
--- app/src/main/kotlin/io/imhungry/firebase/repository/FirebaseVisitRepository.kt
+++ app/src/main/kotlin/io/imhungry/firebase/repository/FirebaseVisitRepository.kt
@@ -8,12 +8,26 @@ class FirebaseVisitRepository @Inject constructor(
     db: FirebaseFirestore
 ) : FirebaseBaseCollectionRepository(db.collection(FirebaseVisit.FIREBASE_COLLECTION_NAME_VISITS)) {
 
-    suspend fun getPlaceVisits(placesId: String, limit: Long = FIREBASE_VISIT_DEFAULT_LIMIT) =
+    private suspend fun queryVisits(field: String, value: String, limit: Long, startAt: Long) =
         performListQuery<FirebaseVisit>(
             collection.whereEqualTo(
-                FirebaseVisit::placesId.name,
-                placesId
-            ).limit(limit).get()
+                field,
+                value
+            ).limit(limit).startAt(startAt).get()
+        )
+
+    suspend fun getPlaceVisits(placesId: String, limit: Long = FIREBASE_VISIT_DEFAULT_LIMIT, startAt: Long = 0) =
+        queryVisits(FirebaseVisit::placesId.name, placesId, limit, startAt)
+
+    suspend fun getLeaderVisits(userId: String, limit: Long = FIREBASE_VISIT_DEFAULT_LIMIT, startAt: Long = 0) =
+        queryVisits(FirebaseVisit::leader.name, userId, limit, startAt)
+
+    suspend fun getUserVisits(userId: String, limit: Long = FIREBASE_VISIT_DEFAULT_LIMIT, startAt: Long = 0) =
+        performListQuery<FirebaseVisit>(
+            collection.whereArrayContains(
+                FirebaseVisit::users.name,
+                userId
+            ).limit(limit).startAt(startAt).get()
         )
 
     companion object {
