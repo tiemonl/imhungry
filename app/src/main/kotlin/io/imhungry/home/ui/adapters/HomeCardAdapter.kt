f8d4e4e9925decc672254b2ffe312f2f7dc74776
Lucas Combs <l-combs@hotmail.com>
2019-04-17 21:12:30 -0400

    GH-46 | Cleanup and optimization for home adapters

--- app/src/main/kotlin/io/imhungry/home/ui/adapters/HomeCardAdapter.kt
+++ app/src/main/kotlin/io/imhungry/home/ui/adapters/HomeCardAdapter.kt
@@ -8,14 +8,14 @@ import androidx.recyclerview.widget.RecyclerView
 import io.imhungry.R
 import java.util.*
 
-class HomeCardAdapter(private val resturants: ArrayList<ArrayList<String>>, private val context: Context) :
+class HomeCardAdapter(private val resturant_lists: ArrayList<ArrayList<String>>, private val context: Context) :
     RecyclerView.Adapter<HomeCardAdapter.HomeViewHolder>() {
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder.
     // Each data item is just a string in this case that is shown in a TextView.
-    class HomeViewHolder(val recycler: RecyclerView) : RecyclerView.ViewHolder(recycler)
+    class HomeViewHolder(val resturantListsRecycler: RecyclerView) : RecyclerView.ViewHolder(resturantListsRecycler)
 
 
     // Create new views (invoked by the layout manager)
@@ -24,21 +24,21 @@ class HomeCardAdapter(private val resturants: ArrayList<ArrayList<String>>, priv
         viewType: Int
     ): HomeCardAdapter.HomeViewHolder {
         // create a new view
-        val recycler = LayoutInflater.from(parent.context)
+        val ReturantsRecycler = LayoutInflater.from(parent.context)
             .inflate(R.layout.resturant_list, parent, false) as RecyclerView
         // set the view's size, margins, paddings and layout parameters
 
-        return HomeViewHolder(recycler)
+        ReturantsRecycler.layoutManager = LinearLayoutManager(this.context, RecyclerView.HORIZONTAL, false)
+        return HomeViewHolder(ReturantsRecycler)
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.recycler.adapter = ResturantScrollerAdapter(resturants[position])
-        holder.recycler.layoutManager = LinearLayoutManager(this.context, RecyclerView.HORIZONTAL, false)
+        holder.resturantListsRecycler.swapAdapter(RestaurantsScrollerAdapter(resturant_lists[position]), true)
     }
 
     // Return the size of your dataset (invoked by the layout manager)
-    override fun getItemCount() = resturants.size
+    override fun getItemCount() = resturant_lists.size
 }
