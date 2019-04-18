fdf72cfe737d069f743d40d6dc80f3d8bb07acb4
Lucas Combs <l-combs@hotmail.com>
2019-04-17 21:12:30 -0400

    GH-46 | Cleanup and optimization for home adapters

--- app/src/main/kotlin/io/imhungry/home/ui/adapters/RestaurantsScrollerAdapter.kt
+++ app/src/main/kotlin/io/imhungry/home/ui/adapters/RestaurantsScrollerAdapter.kt
@@ -6,34 +6,35 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import io.imhungry.R
 
-class ResturantScrollerAdapter(private val resturants: ArrayList<String>) :
-    RecyclerView.Adapter<ResturantScrollerAdapter.ResturantPreviewHolder>() {
+class RestaurantsScrollerAdapter(private val resturants: ArrayList<String>) :
+    RecyclerView.Adapter<RestaurantsScrollerAdapter.RestaurantPreviewHolder>() {
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder.
     // Each data item is just a string in this case that is shown in a TextView.
-    class ResturantPreviewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    class RestaurantPreviewHolder(val restaurantNameTextView: TextView) :
+        RecyclerView.ViewHolder(restaurantNameTextView)
 
 
     // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): ResturantScrollerAdapter.ResturantPreviewHolder {
+    ): RestaurantsScrollerAdapter.RestaurantPreviewHolder {
         // create a new view
         val textView = LayoutInflater.from(parent.context)
             .inflate(R.layout.resturant_name_card, parent, false) as TextView
         // set the view's size, margins, paddings and layout parameters
 
-        return ResturantPreviewHolder(textView)
+        return RestaurantPreviewHolder(textView)
     }
 
     // Replace the contents of a view (invoked by the layout manager)
-    override fun onBindViewHolder(holder: ResturantPreviewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RestaurantPreviewHolder, position: Int) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.textView.text = resturants[position]
+        holder.restaurantNameTextView.text = resturants[position]
     }
 
     // Return the size of your dataset (invoked by the layout manager)
