2ce3eeada599da36388331049e7ceaa314dc8ec9
Liam <tiemon.liam@gmail.com>
2019-04-20 23:17:54 -0400

    GH-29: reworked fragment navigation

--- app/src/main/kotlin/io/imhungry/settings/SettingsFragment.kt
+++ app/src/main/kotlin/io/imhungry/settings/SettingsFragment.kt
@@ -7,12 +7,12 @@ import com.google.firebase.auth.FirebaseAuth
 import io.imhungry.R
 import io.imhungry.login.launchLoginActivity
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment(private val preferenceResourceId: Int) : PreferenceFragmentCompat() {
 
     private val firebaseAuth by lazy { FirebaseAuth.getInstance() }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.preferences, rootKey)
+        setPreferencesFromResource(preferenceResourceId, rootKey)
 
         findPreference(getString(R.string.settings_user_state))?.let {
             val user = firebaseAuth.currentUser
@@ -36,7 +36,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
         }
 
         findPreference(getString(R.string.settings_app_theme))?.let {
-            it.setOnPreferenceChangeListener {_, _ ->
+            it.setOnPreferenceChangeListener { _, _ ->
                 activity?.finish()
                 activity?.startActivity(activity?.intent)
                 true
@@ -44,39 +44,24 @@ class SettingsFragment : PreferenceFragmentCompat() {
         }
     }
 
-
     override fun onPreferenceTreeClick(preference: Preference): Boolean {
-        return when (preference.key) {
-            getString(R.string.settings_category_restaurant_filters_key) -> {
-                val ft = fragmentManager!!.beginTransaction()
-                ft.replace(R.id.settingsContainer, SettingsDetails(), getString(R.string.settings_category_restaurant_filters_key))
-                ft.commit()
-                true
-            } getString(R.string.settings_category_notifications_key) -> {
-                val ft = fragmentManager!!.beginTransaction()
-                ft.replace(R.id.settingsContainer, SettingsDetails(), getString(R.string.settings_category_notifications_key))
-                ft.commit()
-                true
-            } getString(R.string.settings_category_general_key) -> {
-                val ft = fragmentManager!!.beginTransaction()
-                ft.replace(R.id.settingsContainer, SettingsDetails(), getString(R.string.settings_category_general_key))
-                ft.commit()
-                true
-            }  getString(R.string.settings_category_security_key) -> {
-                val ft = fragmentManager!!.beginTransaction()
-                ft.replace(R.id.settingsContainer, SettingsDetails(), getString(R.string.settings_category_security_key))
-                ft.commit()
-                true
-            }   getString(R.string.settings_category_account_key) -> {
-                val ft = fragmentManager!!.beginTransaction()
-                ft.replace(R.id.settingsContainer, SettingsDetails(), getString(R.string.settings_category_account_key))
-                ft.commit()
-                true
-            } else -> {
+        when (preference.key) {
+
+            getString(R.string.settings_category_restaurant_filters_key) -> launchPreferenceActivity(R.xml.preferences_restaurant_filters)
+            getString(R.string.settings_category_notifications_key) -> launchPreferenceActivity(R.xml.preferences_notifications)
+            getString(R.string.settings_category_general_key) -> launchPreferenceActivity(R.xml.preferences_general)
+            getString(R.string.settings_category_security_key) -> launchPreferenceActivity(R.xml.preferences_security_and_location)
+            getString(R.string.settings_category_account_key) -> launchPreferenceActivity(R.xml.preferences_account)
+
+            else -> {
                 super.onPreferenceTreeClick(preference)
+                return false
             }
         }
+        return true
     }
 
-
+    private fun launchPreferenceActivity(resourceId: Int) = activity?.let {
+        it.startActivity(SettingsActivity.buildIntentFromPreferences(it, resourceId))
+    }
 }
