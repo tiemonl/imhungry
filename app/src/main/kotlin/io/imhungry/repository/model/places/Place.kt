65cf095640eabdf2a7b2d291a07a941db3126ab7
TripleFrequency <triplefrequency@triplefrequency.com>
2019-04-20 23:17:44 -0400

    GH-83: Continue work on place repo and model

--- app/src/main/kotlin/io/imhungry/repository/model/places/Place.kt
+++ app/src/main/kotlin/io/imhungry/repository/model/places/Place.kt
@@ -1,5 +1,16 @@
 package io.imhungry.repository.model.places
 
+import com.google.maps.model.LatLng
+import java.net.URL
+
 data class Place(
-    val placesId: String
+    val placeId: String,
+    val location: LatLng,
+    val iconUrl: URL,
+    val name: String,
+    val isOpen: Boolean,
+    val rating: Float,
+    val types: List<String>,
+    val photoReferences: List<String>,
+    val address: String
 )
