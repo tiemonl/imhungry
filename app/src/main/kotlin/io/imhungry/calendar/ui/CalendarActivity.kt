7e54671bfa1f1db0431bd891f9456d5c39658259
tiemonl <tiemon.liam@gmail.com>
2019-05-01 20:05:50 -0400

    GH78: date showing in calendar correctly

--- app/src/main/kotlin/io/imhungry/calendar/ui/CalendarActivity.kt
+++ app/src/main/kotlin/io/imhungry/calendar/ui/CalendarActivity.kt
@@ -2,13 +2,13 @@ package io.imhungry.calendar.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.CalendarContract
 import android.provider.CalendarContract.Events.*
 import android.widget.CalendarView
-import androidx.appcompat.app.AppCompatActivity
 import io.imhungry.R
-import org.threeten.bp.LocalDate
-import org.threeten.bp.ZoneId
 import io.imhungry.common.ui.NavigationActivity
+import java.util.*
+
 
 class CalendarActivity : NavigationActivity() {
 
@@ -17,14 +17,18 @@ class CalendarActivity : NavigationActivity() {
         setContentView(R.layout.activity_calendar)
 
         val calendarView = findViewById<CalendarView>(R.id.calendarView)
-        calendarView?.setOnDateChangeListener { _, year, month, dayOfMonth ->
-            val zone = ZoneId.systemDefault()
-            val date = LocalDate.of(year,month+1, dayOfMonth).atStartOfDay(zone).toEpochSecond()
+        calendarView?.setOnDateChangeListener { _, year, month, dayOfMonth ->git st
+            val beginTime = Calendar.getInstance()
+            beginTime.set(year, month, dayOfMonth, 12, 0)
+            val endTime = Calendar.getInstance()
+            endTime.set(year, month, dayOfMonth, 13, 0)
             val intent = Intent(Intent.ACTION_INSERT)
                 .setData(CONTENT_URI)
                 .putExtra(TITLE, "Lunch at Location")
                 .putExtra(EVENT_LOCATION, "Location")
-                .putExtra(DTSTART, date)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.timeInMillis)
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.timeInMillis)
+
             startActivityForResult(intent, 0)
         }
     }
